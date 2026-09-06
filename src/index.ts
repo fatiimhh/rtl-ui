cc0148455b3553786ce35960cfd12da605b620a5
Fatima Bani Sakher <fatima.mbk00@gmail.com>
2026-09-06 19:19:22 +0300

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { Button } from "./components/Button/Button";
+export type { ButtonProps } from "./components/Button/Button";
+
+export { Input } from "./components/Input/Input";
+export type { InputProps } from "./components/Input/Input";
+
+export { Card } from "./components/Card/Card";
+export type { CardProps } from "./components/Card/Card";
+
+export { Modal } from "./components/Modal/Modal";
+export type { ModalProps } from "./components/Modal/Modal";
+
+export { Dropdown } from "./components/Dropdown/Dropdown";
+export type { DropdownProps, DropdownOption } from "./components/Dropdown/Dropdown";
+
+export { Toast } from "./components/Toast/Toast";
+export type { ToastProps } from "./components/Toast/Toast";
