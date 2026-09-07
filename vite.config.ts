bee603ee46e1929c8faf37d4d96e60fad0a653e5
Fatima Bani Sakher <fatima.mbk00@gmail.com>
2026-09-07 17:54:07 +0300

    vite.config.ts update

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
     react(),
-    dts({
-      include: ["src"],
-      exclude: ["src/**/*.stories.tsx"],
-    }),
+    
   ],
   build: {
     lib: {
